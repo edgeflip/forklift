bf1bb8ebbba86abead11d6563f387f8a1fb89e65
Tristan Crockett <tristan@edgeflip.com>
2014-05-01 15:26:26 -0500

    Use correct environment for celery

--- fab/serve.py
+++ fab/serve.py
@@ -1,6 +1,7 @@
 """Fabric tasks for managing servers"""
 from fabric import api as fab
+import os
 
 @fab.task(name='celery')
-def start_celery(env='development'):
-    fab.local("ENV={} celery -A forklift.tasks worker".format(env))
+def start_celery():
+    fab.local("ENV={} celery -A forklift.tasks worker".format(os.environ['ENV']))
