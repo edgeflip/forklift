0b17dfe91c4f2817281fbdaa3e93a25a5b9c039a
Tristan Crockett <tristan@edgeflip.com>
2014-07-10 10:20:58 -0500

    FBSync import scripts: Fix, declare argument requirements, add useful descriptions, tweak formatting

--- forklift/scripts/import_fbsync.py
+++ forklift/scripts/import_fbsync.py
@@ -1,28 +1,37 @@
-import logging
+import argparse
 import os
 import sys
 sys.path.append(os.path.abspath(os.curdir))
 from forklift.db.utils import checkout_raw_connection
 from forklift.loaders.fbsync import load_and_dedupe, POSTS_TABLE, USER_POSTS_TABLE
 
-logger = logging.getLogger(__name__)
-
-
-###################################
-# use this if we have changed how the CSVs are created and thus need to blow away
-# the entire existing tableset
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Replace the FBSync tables (posts, user_posts) with freshly populated ones')
-    parser.add_argument('--bucket_name', type=str, help='S3 bucket in which the fbsync data resides')
-    parser.add_argument('--posts_folder', type=str, help='S3 folder (excluding the bucket) in which the posts reside. Can be nested (e.g. rstransfer/posts)'
-    parser.add_argument('--user_posts_folder', type=str, help='S3 folder (excluding the bucket) in which the user_posts reside. Can be nested (e.g. rstransfer/links)')
+    parser = argparse.ArgumentParser(description='Replace the FBSync tables (posts, user_posts) with freshly populated ones. Data for each table must be in proper TSV format, in the same column order as its respective table, before running')
+    parser.add_argument(
+        '--bucket_name',
+        type=str,
+        required=True,
+        help='S3 bucket in which the fbsync data resides'
+    )
+    parser.add_argument(
+        '--posts_folder',
+        type=str,
+        required=True,
+        help='S3 folder (excluding the bucket) in which the posts reside. Can be nested (e.g. rstransfer/posts)'
+    )
+    parser.add_argument(
+        '--user_posts_folder',
+        type=str,
+        required=True,
+        help='S3 folder (excluding the bucket) in which the user_posts reside. Can be nested (e.g. rstransfer/links)'
+    )
     args = parser.parse_args()
 
     bucket_name = args.bucket_name
     posts_folder = args.posts_folder
     user_posts_folder = args.user_posts_folder
 
-    with checkout_raw_connection as connection:
+    with checkout_raw_connection() as connection:
         load_and_dedupe(bucket_name, posts_folder, POSTS_TABLE, connection, optimize=True)
         load_and_dedupe(bucket_name, user_posts_folder, USER_POSTS_TABLE, connection, optimize=True)
--- forklift/scripts/import_fbsync_incremental.py
+++ forklift/scripts/import_fbsync_incremental.py
@@ -6,14 +6,26 @@ from forklift.loaders.fbsync import add_new_data
 from forklift.db.utils import checkout_raw_connection
 
 
-###################################
-# when FBSync just grabbed some new data and we want to merge it in
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Add new data to the FBSync tables (posts, user_posts)')
-    parser.add_argument('--bucket_name', type=str, help='S3 bucket in which the new data resides')
-    parser.add_argument('--posts_folder', type=str, help='S3 folder (excluding the bucket) in which the posts reside. Can be nested (e.g. rstransfer/posts)')
-    parser.add_argument('--user_posts_folder', type=str, help='S3 folder (excluding the bucket) in which the user_posts reside. Can be nested (e.g. rstransfer/links)')
+    parser = argparse.ArgumentParser(description='Add new data to the FBSync tables (posts, user_posts). Data for each table must be in proper TSV format, in the same column order as its respective table, before running.')
+    parser.add_argument(
+        '--bucket_name',
+        type=str,
+        required=True,
+        help='S3 bucket in which the new data resides'
+    )
+    parser.add_argument(
+        '--posts_folder',
+        type=str,
+        required=True,
+        help='Path to S3 folder or file (excluding the bucket) in which the posts reside. Can be nested (e.g. rstransfer/posts)'
+    )
+    parser.add_argument(
+        '--user_posts_folder',
+        type=str,
+        required=True,
+        help='Path to S3 folder or file (excluding the bucket) in which the user_posts reside. Can be nested (e.g. rstransfer/links)'
+    )
     args = parser.parse_args()
 
     bucket_name = args.bucket_name
