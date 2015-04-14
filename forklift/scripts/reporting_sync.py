67c97d0cd20ea794d9c6c01b1236d3b9fa294a1e
Tristan Crockett <tristan@edgeflip.com>
2015-04-14 18:26:43 -0500

    Some more v1 scripts needed to go

--- forklift/scripts/reporting_sync.py
+++
@@ -1,5 +0,0 @@
-from forklift.db.base import rds_source_engine, redshift_engine, rds_cache_engine
-from forklift.loaders.reporting import process
-
-if __name__ == '__main__':
-    process(rds_source_engine, redshift_engine, rds_cache_engine)
